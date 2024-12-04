7793f8b93c7ddf093c29e8e7eed184b0b051d478
Enrique Angrisano <milangrisano@gmail.com>
2024-12-03 20:23:22 -0500

    updated content

--- lib/content/info_swiper.dart
+++ lib/content/info_swiper.dart
@@ -25,41 +25,51 @@ List<InfoSwiper> swiperContent = [
   InfoSwiper(
     imagen: "assets/images/Artivela4.jpeg",
     title: "Pasadia enVelero",
-    descripcion: "\$250.000 p/p INCLUYE: ENTRADA A LA MARINA, SEGURO, SNACKS, BEBIDAS NO ALCOHÓLICAS , ALMUERZO, PADDLEBOARD Y EQUIPO DE SNORKEL",
+    descripcion: "\$250.000 p/p Incluye: Entrada a la marina, seguro, snacks, bebidas no alcoholicas, almuerzo, paddleboard y equipo de snorkel",
   ),
   InfoSwiper(
     imagen: "assets/images/Artivela4.jpeg",
-    title: "Pasadia en Velero",
-    descripcion: "Itinerario: Nos encontraremos a las 10:30 en la Marina Internacional de Santa Marta, bordaremos el Artivela, nuestro velero. A las 11 am saldremos rumbo a nuestro destino Bahia Concha en el Parque Tayrona o alguna bahía del Tayrona.\nLa navegación hasta nuestro destino tardará aprox 1.5 horas. Pondremos nuestras cañas para ir pescando. Pasaremos frente a la bahía de Taganga y de Granate. Allí empezaremos el famoso paso de Isla Aguja, la única isla del Parque Tayrona. Una vez pasemos Isla Aguja entraremos en Bahía Concha y trataremos de anclar lo más cerca de la playa. Nuestros compañeros de experiencia pueden elegir entre nadar, caminar por la playa, hacer snorkel o simplemente asolearse en el barco.\nA las 3:30 pm comenzaremos nuestro regreso a Santa Marta. Navegaremos a vela y todos participaràn. Llegaremos a la Marina entre 5:00 y 5:30pm. Si eres propenso a los mareos puedes tomar algunas medicinas para prevenirlo.",
+    title: "Navega en Catamarán \nBahía Concha",
+    descripcion: "Navegar por el Tayrona es una experiencia única que nunca olvidarás. Podrás admirar las montañas costeras más altas del mundo desde el mar. Mar donde los ríos que provienen de los picos nevados de la Sierra desembocan en el Mar Caribe. Nos encontraremos a las 9:00 am en la Marina Internacional de Santa Marta. A las 9:30 zarparemos rumbo a nuestro destino. Tan pronto salgamos de la Bahía de Santa Marta entraremos en la Bahía de Taganga, pueblo de pescadores donde el turismo se ha fusionado con los locales. Seguiremos nuestra navegación entrando al Parque Tayrona y pasaremos por la Bahía de Granate donde se puede observar la faena de pesca artesanal y donde las escuelas de buceo suelen ir. Pasaremos Isla aguja y playa Brava para entrar a nuestro destino en Bahía Concha, donde nadaremos, remaremos, practicaremos snorkel o simplemente descansaremos. Deleitarnos nuestro increíble almuerzo a base de pescado muy fresco (Ceviche) (hay opción para vegetarianos) A las 3:30pm iniciaremos el regreso a la Marina. Esta vez lo haremos navegando a vela. Estaremos de regreso alrededor de las 4:30pm.",
+  ),
+  InfoSwiper(
+    imagen: "assets/images/Snorlkel4.jpeg",
+    title: "Atardecer Navegando a Vela en Catamarán",
+    descripcion: "Nos encontraremos a las 4:30 pm en la Marina Internacional de Santa Marta, donde comenzará nuestra aventura. A las 5:00 pm, zarparemos hacia El Rodadero, pasando por las encantadoras playas de Lipe, Santo Cristo, el Hoyo e Inca Inca. En el trayecto, podrás deleitarte con el relajante sonido del mar desde la proa o disfrutar de música desde el cockpit. A medida que avanzamos, descubrirás paisajes deslumbrantes y una atmósfera vibrante llena de sonidos alegres que te acompañarán durante toda la travesía. Con una refrescante bebida en mano, contemplaremos juntos la espectacular puesta de sol, mientras escuchamos la música que nos inspire y nos conecte. Navegaremos sin prisas, saboreando cada momento de esta experiencia única e inolvidable. Aspectos destacados: Canta y baila si lo deseas; lo importante es que disfrutes cada instante y agregues un toque especial a tu noche. A bordo de nuestro barco, te sentirás como en casa, con un ambiente acogedor y lleno de buenas vibras. ¡Ven y vive una noche mágica con nosotros! Itinerario Atardecer navegando a vela encatamarán \n4:30pm a 8:00pm",
   ),
   InfoSwiper(
     imagen: "assets/images/Snorlkel4.jpeg",
     title: "Snorkel en compañía de locales",
-    descripcion: "Itinerario: Nos encontramos en la Marina internacional de Santa Marta a las 8:00am o 2:00pm (tenemos dos salidas al día) para abordar nuestra lancha de 18 pies por aproximadamente 5 minutos con destino a nuestra primera parada el punta Betín, donde haremos snorkel, aquí observaremos diferentes especies de peces de colores como el Ángel Reina, pez loro y con suerte algunos pulpos después de unas sesión de 30 minutos subiremos a a la lancha para ir a nuestro segundo punto el famoso morro de Santa Marta donde tendremos otra sesión de careteo de 30 minutos observando gran variedad de corales, como coral cerebro entre otros. subiremos nuevamente a nuestra lancha para regresar a la Marina, donde conversaremos acerca de la vida marina, tomaremos un snack y un delicioso smoothie o jugo de fruta de tu preferencia.",
+    descripcion: "Itinerario: Nos encontramos en la Marina internacional de Santa Marta a las 8:00am o 2:00pm (tenemos dos salidas al día) para abordar nuestra lancha de 18 pies por aproximadamente 5 minutos con destino a nuestra primera parada el punta Betín, donde haremos snorkel, aquí observaremos diferentes especies de peces de colores como el Ángel Reina, pez loro y con suerte algunos pulpos después de unas sesión de 30 minutos subiremos a a la lancha para ir a nuestro segundo punto el famoso morro de Santa Marta donde tendremos otra sesión de careteo de 30 minutos observando gran variedad de corales, como coral cerebro entre otros. subiremos nuevamente a nuestra lancha para regresar a la Marina, donde conversaremos acerca de la vida marina, tomaremos un snack y un delicioso smoothie o jugo de fruta de tu preferencia. \n8:00 am a 10:00 am y 2:00pm a 4:00pm",
   ),
   InfoSwiper(
     imagen: "assets/images/Artivela3.jpeg",
-    title: "Amanecer de pesca",
-    descripcion: "Itinerario: Nos veremos a las 6:30 am en la puerta de la Marina Internacional de Santa Marta. Allí abordaremos nuestra lancha de 27 de pies perfecta para una faena de pesca.\nPondremos rumbo con nuestras cañas en el agua a la isla de la Aguja en el Parque Nacional Tayrona. En este recorrido pasaras por enfrente de la bahía de Taganga disfrutando del bello amanecer Samario. Si tenemos suerte podremos pescar desde un bonito, atún pequeño, hasta dorados y atunes muy grandes.\nAprovecharemos el tiempo navegando para tener un delicioso café de la sierra y deleitar algunas frutas locales. La pesca del día se la podrán llevarlo compartirla con la tripulación para sus familias. A las 11 am estaremos de regreso en la Marina no sin antes haber nadado un rato en el mar.",
+    title: "Amanecer de pesca Artesanal",
+    descripcion: "Itinerario: Nos encontraremos a las 5:00 am en la entrada de la Marina Internacional de Santa Marta, donde nuestro día comienza con el amanecercaribeño como fondo. Subiremos a nuestra lancha de 27 pies, ideal para la jornada de pesca que nos espera. Una vez a bordo, zarparemos hacia nuestra primera parada, a unos 40 minutos de distancia. Con el equipo listo, lanzaremos el palangre previamente preparado al mar, mientras el sol se alza sobre el horizonte. Pero aquí no termina la acción: mientras el palangre hace su trabajo, aprovecharemos el tiempo troleando con nuestras cañas de pescar durante aproximadamente una hora, en busca de más pesca. Después, regresaremos a la primera parada para recoger el palangre. Este es un momento que requiere paciencia, pero la recompensa bien puede ser un pargo, un mero, un medegral, o incluso un atún. Recuerda, la pesca es una mezcla de paciencia y un toque de suerte. Durante la travesía, disfrutaremos de un delicioso café de la Sierra Nevada, uno sándwiches y frutas frescas locales. Además, compartimos la pesca del día con la tripulación, contribuyendo así a sus hogares y haciendo de esta experiencia algo aún más especial. Alrededor de la 1:00 pm, regresaremos a la Marina, pero antes haremos una parada para un refrescante baño en las aguas cristalinas del Caribe.",
   ),
   InfoSwiper(
     imagen: "assets/images/Landing2.jpeg",
-    title: "Santo Cristo: Paraíso Escondido",
-    descripcion: "Itinerario: Impensable entre Santa Marta y Rodadero, nos encontraremos a las 8:30am en la marina internacional de Santa Marta, para a tomar nuestra lancha por unos 10 minutos aprox. Con destino a Santo Cristo, una pequeña bahía que posee formaciones rocosas que crean una cristalina piscina natural. Disfrutaremos el día en completa calma, la privacidad y la tranquilidad te permitirán leer un libro, descansar, nadar o simplemente tomar el sol, al medio almorzaremos. sobre las 2:00pm estaremos e de regreso a nuestro punto de salida. Volviendo recargados después de haber disfrutado de un paraíso lejos de las concurridas playas de la ciudad"
+    title: "Cultura y diversión: Don Diego",
+    descripcion: "Itinerario: El transporte los buscará para salir de Santa Marta entre 7:00 y 8 am (depende del lugar de hospedaje).Nuestra primera parada, hacia las 9:30 am, será a orillas del Río Don Diego, que nace en la Sierra Nevada. Caminaremos por 25 minutos viendo vegetación hermosa y peculiar (como las plantas dormilonas), animales únicos y las ruinas de un asentamiento Tayrona (es casi como ciudad perdida, pero más pequeña) . Al terminar dicho recorrido a, hacia las 10:15 bajaremos a un ritmo lento y relajante por el río durante 45 minutos aproximadamente viendo babillas, garzas azules, negras, pájaros de la zona, micos aulladores y llegaremos al lugar donde el río se encuentra con el mar. Si tenemos suerte desde allí veremos los picos nevados de la Sierra de Santa Marta. Sentiremos el agua fría del uno y la calidez del agua salada. iremos río arriba para disfrutar de un delicioso almuerzo típico a la orilla del río, donde podremos ver de cerca los monos aulladores y otras especies. Pasaremos una tarde tranquila. Regresaremos a las 4:30 pm reenergizados, eso es seguro"
   ),
   InfoSwiper(
-    imagen: "assets/images/bote2.jpeg",
-    title: "Ciénaga Grande, Descubriendo la Magia de los palafitos",
-    descripcion: "Comienza el día temprano para aprovechar el amanecer abordaremos rumbo hacia Ciénaga Grande, el complejo lagunar costero más grande de Colombia y uno de los más estratégicos para la vida en el planeta. Llegaremos al puente de la barra que comunica Barranquilla y Santa Marta e iniciaremos el recorrido por la Ciénaga Grande durante una hora y media, explorando la belleza natural del lugar y observando la flora y fauna de la región (Garzas, patos, Cormorán, bocachico, sábalos, entre otros), pasando por Buenavista hasta llegar a Nueva Venecia para conocer su historia, su arquitectura única y estilo de vida tradicional. Interactuaremos con los lugareños para aprender sobre sus costumbres, tradiciones y artesanías locales también veremos los grafitis que son el arte que pinta las casas sobre el agua, al medio día comenzaremos nuestro regreso hasta Santa Marta llegaremos a un restaurante frente al mar para almorzar y tomar un baño de mar si lo deseamos, de nuevo abordaremos para terminar nuestro recorrido en la marina internacional de Santa Marta.",
+    imagen: "assets/images/Landing2.jpeg",
+    title: "Paso del Mango: cascadas,experiencia de Cacaoyreserva natural",
+    descripcion: "Itinerario: Paso del Mango se encuentra localizado en el Parque Nacional Sierra Nevada de Santa Marta, atravesando la antigua ciudad indígena de Masinga. En este increíble espacio natural conoceremos diferentes tipos de ecosistemas como Bosque Seco Tropical, Bosque Nublado, Bosque Primario y Bosque Secundario. Realizaremos tres actividades centrales: Trekking a tres cascadas localizadas en la selva, Visitaremos una reserva de animales en peligro de extinción y un programa exitoso de reforestación, fabricaremos nuestro propio chocolate luego de aprender todo sobre el cultivo de cacao y nos haremos mascarillas faciales del producto. El almuerzo lo tomaremos en un restaurante en la selva en donde los ingredientes son locales, orgánicos y sostenibles. Ven y disfruta con nosotros de una aventura única en una de las zonas menos exploradas de la región donde veremos indígenas, petroglifos, diversidad de especies nativas de flora y fauna en un pequeño paraíso en la montaña."
   ),
   InfoSwiper(
-    imagen: "assets/images/proa_view.png",
-    title: "Atardecer en Velero",
-    descripcion: "",
-  ),
+    imagen: "assets/images/Landing2.jpeg",
+    title: "charter: elige el destino y la embarcación",
+    descripcion: ""
+  ),  
   InfoSwiper(
-    imagen: "assets/images/Tequila2.jpeg",
-    title: "Alquiler por Horas",
-    descripcion: "",
-  ),
+    imagen: "assets/images/Landing2.jpeg",
+    title: "compromiso abordo: velero",
+    descripcion: ""
+  ),  
+  InfoSwiper(
+    imagen: "assets/images/Landing2.jpeg",
+    title: "alquiler de embarcación por horas",
+    descripcion: ""
+  ),  
 ];
